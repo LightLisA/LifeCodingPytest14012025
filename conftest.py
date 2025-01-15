a66a35bd3ff9123ca8171f5590e37afa7ab35479
Oleksii Lysytskyi <o.lysytskyi@aibuy.io>
2025-01-15 03:14:11 +0200

    made changes - commit 12

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ def browser(request):
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size:1920,1080")
+    options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
     yield driver
